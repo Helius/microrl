5545b45dabca1dac64193c1b578f2292e44ef217
eugene <ghelius@gmail.com>
2011-09-13 14:54:13 +0700

    make demo more demonstrately

--- microrl_tst.c
+++ microrl_tst.c
@@ -8,9 +8,32 @@
 
 #define DBG(...) printf("\033[33m");printf(__VA_ARGS__);printf("\033[0m");
 
-#define _NUM_OF_CMD 5
-char * keyworld [] = {"fuck","help","list","function", "lisp"};
+// definition commands word
+#define _CMD_HELP  "help"
+#define _CMD_CLEAR "clear"
+#define _CMD_LIST  "list"
+#define _CMD_LISP  "lisp" // for demonstration completion on 'l + <TAB>'
+#define _CMD_NAME  "name"
+#define _CMD_VER   "version"
+// sub commands for version command
+	#define _SCMD_MRL  "microrl"
+	#define _SCMD_DEMO "demo"
+
+#define _NUM_OF_CMD 6
+#define _NUM_OF_VER_SCMD 2
+
+//available  commands
+char * keyworld [] = {_CMD_HELP, _CMD_CLEAR, _CMD_LIST, _CMD_NAME, _CMD_VER, _CMD_LISP};
+// version subcommands
+char * ver_keyworld [] = {_SCMD_MRL, _SCMD_DEMO};
+
+// array for comletion
 char ** compl_world [_NUM_OF_CMD + 1];
+
+// 'name' var for store some string
+#define _NAME_LEN 8
+char name [_NAME_LEN];
+int val;
 
 //*****************************************************************************
 // print callback for microrl library
@@ -22,31 +45,64 @@ void print (char * str)
 //*****************************************************************************
 void print_help ()
 {
-	print ("Use TAB key for completion\nCommand:\n\thelp - this message\n\tlist - list all commands in tree\n");
+	print ("Use TAB key for completion\n\rCommand:\n\r");
+	print ("\tversion {microrl | demo} - print version of microrl lib or version of this demo src\n\r");
+	print ("\thelp  - this message\n\r");
+	print ("\tclear - clear screen\n\r");
+	print ("\tlist  - list all commands in tree\n\r");
+	print ("\tname [string] - print 'name' value if no 'string', set name value to 'string' if 'string' present\n\r");
+	print ("\tlisp - dummy command for demonstation auto-completion, while inputed 'l+<TAB>'\n\r");
 }
 
 //*****************************************************************************
 // execute callback for microrl library
-int execute (int argc, const char * const * tkn_arr)
+// do what you want here, but don't write to argv!!! read only!!
+int execute (int argc, const char * const * argv)
 {
 	int i = 0;
+	// just iterate through argv word and compare it with your commands
 	while (i < argc) {
-		if (strcmp (tkn_arr[i], "help") == 0) {
-			print ("microrl library based shell v 1.0\n");
-			print_help ();
-			return 1;
-		} else if (strcmp (tkn_arr[i], "list") == 0) {
-			print ("available command:\n");
+		if (strcmp (argv[i], _CMD_HELP) == 0) {
+			print ("microrl library based shell v 1.0\n\r");
+			print_help ();        // print help
+		} else if (strcmp (argv[i], _CMD_NAME) == 0) {
+			if ((++i) < argc) { // if value preset
+				if (strlen (argv[i]) < _NAME_LEN) {
+					strcpy (name, argv[i]);
+				} else {
+					print ("name value too long!\n\r");
+				}
+			} else {
+				print (name);
+				print ("\n\r");
+			}
+		} else if (strcmp (argv[i], _CMD_VER) == 0) {
+			if (++i < argc) {
+				if (strcmp (argv[i], _SCMD_DEMO) == 0) {
+					print ("demo v 1.0\n\r");
+				} else if (strcmp (argv[i], _SCMD_MRL) == 0) {
+					print ("microrl v 1.2\n\r");
+				} else {
+					print (argv[i]);
+					print (" wrong argument, see help\n\r");
+				}
+			} else {
+				print ("version needs 1 parametr, see help\n\r");
+			}
+		} else if (strcmp (argv[i], _CMD_CLEAR) == 0) {
+			print ("\033[2J");    // ESC seq for clear entire screen
+			print ("\033[H");     // ESC seq for move cursor at left-top corner
+		} else if (strcmp (argv[i], _CMD_LIST) == 0) {
+			print ("available command:\n");// print all command per line
 			for (int i = 0; i < _NUM_OF_CMD; i++) {
 				print ("\t");
 				print (keyworld[i]);
-				print ("\n");
+				print ("\n\r");
 			}
-			return 1;
 		} else {
 			print ("command: '");
-			print (tkn_arr[i]);
-			print ("' Not found.\n");
+			print (argv[i]);
+			print ("' Not found.\n\r");
 		}
 		i++;
 	}
@@ -59,13 +115,10 @@ char ** complet (int argc, const char * const * argv)
 {
 	int j = 0;
 
-//	for (int i = 0; i < argc; i++) {
-//		DBG ("[%s]\n", argv[i]);
-//	}
 	compl_world [0] = NULL;
 
 	// if there is tocken in cmdline
-	if (argc > 0) {
+	if (argc == 1) {
 		// get last entered tocken
 		char * bit = argv [argc-1];
 		// iterate through our available token and match it
@@ -76,8 +129,14 @@ char ** complet (int argc, const char * const * argv)
 				compl_world [j++] = keyworld [i];
 			}
 		}
-	// if there is no token in cmdline, just print all available token
-	} else { 
+	}	else if ((argc > 1) && (strcmp (argv[0], _CMD_VER)==0)) { // if command needs subcommands
+		// iterate through subcommand for command _CMD_VER array
+		for (int i = 0; i < _NUM_OF_VER_SCMD; i++) {
+			if (strstr (ver_keyworld [i], argv [argc-1]) == ver_keyworld [i]) {
+				compl_world [j++] = ver_keyworld [i];
+			}
+		}
+	} else { // if there is no token in cmdline, just print all available token
 		for (; j < _NUM_OF_CMD; j++) {
 			compl_world[j] = keyworld [j];
 		}
@@ -110,7 +169,6 @@ int main (int argc, char ** argv)
 	// create microrl object and pointer on it
 	microrl_t rl;
 	microrl_t * prl = &rl;
-	DBG ("sizeof (microrl_t)= %d\n", sizeof(microrl_t));
 	// call init with ptr to microrl instance and print callback
 	microrl_init (prl, print);
 	// set callback for execute
