7c0d061f9e181776a3cc36341ead61502092ea30
Stephen Casner <casner@acm.org>
2017-12-14 16:10:23 -0800

    Adapt the example programs to API changes
    Change the example code to include the microrl_t pointer as the first
    argument of all the callbacks as was added to the microrl library in
    commit 7334fbd5a49e517458399cb9ac0c718c806bc4ab.
    
    Note: The changes in avr_misc have not been tested since I don't have
    a build environment.

--- examples/avr_misc/avr_misc.c
+++ examples/avr_misc/avr_misc.c
@@ -50,7 +50,7 @@ void init (void)
 //}
 
 //*****************************************************************************
-void print (const char * str)
+void print (void * pThis, const char * str)
 {
 	int i = 0;
 	while (str [i] != 0) {
@@ -67,24 +67,24 @@ char get_char (void)
 }
 
 //*****************************************************************************
-void print_help (void)
+void print_help (void * pThis, void)
 {
-	print ("Use TAB key for completion\n\rCommand:\n\r");
-	print ("\tclear               - clear screen\n\r");
-	print ("\tset_port port pin   - set 1 port[pin] value, support only 'port_b' and 'port_d'\n\r");
-	print ("\tclear_port port pin - set 0 port[pin] value, support only 'port_b' and 'port_d'\n\r");
+	print (pThis, "Use TAB key for completion\n\rCommand:\n\r");
+	print (pThis, "\tclear               - clear screen\n\r");
+	print (pThis, "\tset_port port pin   - set 1 port[pin] value, support only 'port_b' and 'port_d'\n\r");
+	print (pThis, "\tclear_port port pin - set 0 port[pin] value, support only 'port_b' and 'port_d'\n\r");
 }
 
 //*****************************************************************************
-void set_port_val (unsigned char * port, int pin, int val)
+void set_port_val (void * pThis, unsigned char * port, int pin, int val)
 {
 	if ((*port == PORTD) && (pin < 2) && (pin > 7)) {
-		print ("only 2..7 pin avialable for PORTD\n\r");
+		print (pThis, "only 2..7 pin avialable for PORTD\n\r");
 		return;
 	}
 	
 	if ((*port == PORTB) && (pin > 5)) {
-		print ("only 0..5 pin avialable for PORTB\n\r");
+		print (pThis, "only 0..5 pin avialable for PORTB\n\r");
 		return;
 	}
 
@@ -98,21 +98,21 @@ void set_port_val (unsigned char * port, int pin, int val)
 //*****************************************************************************
 // execute callback for microrl library
 // do what you want here, but don't write to argv!!! read only!!
-int execute (int argc, const char * const * argv)
+int execute (void * pThis, int argc, const char * const * argv)
 {
 	int i = 0;
 	// just iterate through argv word and compare it with your commands
 	while (i < argc) {
 		if (strcmp (argv[i], _CMD_HELP) == 0) {
-			print ("microrl v");
-			print (MICRORL_LIB_VER);
-			print (" library AVR DEMO v");
-			print (_AVR_DEMO_VER);
+			print (pThis, "microrl v");
+			print (pThis, MICRORL_LIB_VER);
+			print (pThis, " library AVR DEMO v");
+			print (pThis, _AVR_DEMO_VER);
 			print("\n\r");
-			print_help ();        // print help
+			print_help (pThis);        // print help
 		} else if (strcmp (argv[i], _CMD_CLEAR) == 0) {
-			print ("\033[2J");    // ESC seq for clear entire screen
-			print ("\033[H");     // ESC seq for move cursor at left-top corner
+			print (pThis, "\033[2J");    // ESC seq for clear entire screen
+			print (pThis, "\033[H");     // ESC seq for move cursor at left-top corner
 		} else if ((strcmp (argv[i], _CMD_SET) == 0) || 
 							(strcmp (argv[i], _CMD_CLR) == 0)) {
 			if (++i < argc) {
@@ -124,29 +124,29 @@ int execute (int argc, const char * const * argv)
 				} else if (strcmp (argv[i], _SCMD_PB) == 0) {
 					port = (unsigned char *)&PORTB;
 				} else {
-					print ("only '");
-					print (_SCMD_PB);
-					print ("' and '");
-					print (_SCMD_PD);
-					print ("' support\n\r");
+					print (pThis, "only '");
+					print (pThis, _SCMD_PB);
+					print (pThis, "' and '");
+					print (pThis, _SCMD_PD);
+					print (pThis, "' support\n\r");
 					return 1;
 				}
 				if (++i < argc) {
 					pin = atoi (argv[i]);
-					set_port_val (port, pin, val);
+					set_port_val (pThis, port, pin, val);
 					return 0;
 				} else {
-					print ("specify pin number, use Tab\n\r");
+					print (pThis, "specify pin number, use Tab\n\r");
 					return 1;
 				}
 			} else {
-					print ("specify port, use Tab\n\r");
+					print (pThis, "specify port, use Tab\n\r");
 				return 1;
 			}
 		} else {
-			print ("command: '");
-			print ((char*)argv[i]);
-			print ("' Not found.\n\r");
+			print (pThis, "command: '");
+			print (pThis, (char*)argv[i]);
+			print (pThis, "' Not found.\n\r");
 		}
 		i++;
 	}
@@ -156,7 +156,7 @@ int execute (int argc, const char * const * argv)
 #ifdef _USE_COMPLETE
 //*****************************************************************************
 // completion callback for microrl library
-char ** complet (int argc, const char * const * argv)
+char ** complet (void * pThis, int argc, const char * const * argv)
 {
 	int j = 0;
 
@@ -196,7 +196,7 @@ char ** complet (int argc, const char * const * argv)
 #endif
 
 //*****************************************************************************
-void sigint (void)
+void sigint (void * pThis)
 {
-	print ("^C catched!\n\r");
+	print (pThis, "^C catched!\n\r");
 }
